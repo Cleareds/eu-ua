5a2e495c527dab44bddccf33320a8e10fd40cb82
Anton Kravchuk anton.kravchuk@vaimo.com <anton.kravchuk@vaimo.com>
2026-03-06 11:35:47 +0100

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-19QC7D2JX0"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-19QC7D2JX0');
+        `}} />
+      </head>
       <body className={`${geistSans.variable} antialiased min-h-screen flex flex-col`} style={{ backgroundColor: "#F8F9FA", color: "#1A1A2E" }}>
         <DisclaimerBanner />
         <Header />
